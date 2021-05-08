5aa43e2f2ab61aca8674b887c269877147c085f2
Carlos Escobar <ecarlosandres@gmail.com>
2021-05-07 19:34:53 -0500

    Initial Commit

---
+++ sshApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sshApp
+//
+//  Created by Carlos Escobar on 7/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sshApp/sshAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sshAppApp.swift
+//  sshApp
+//
+//  Created by Carlos Escobar on 7/05/21.
+//
+
+import SwiftUI
+
+@main
+struct sshAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
